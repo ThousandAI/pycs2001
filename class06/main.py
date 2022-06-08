0178b16b2b01eab3b87ed6d3fa27dccac208ab3d
thousandai <ceo@thousandai.com>
2022-06-08 17:46:38 +0800

    class06 constructor

--- class06/main.py
+++ class06/main.py
@@ -9,6 +9,8 @@ utils.introduce(name="Thousand",age=30,gender="Male")
 print(utils.get_max(number=number))
 print(utils.get_distance(point1=(3,5),point2=(6,1)))
 """
+
+"""
 # Package
 import helper.show as show
 import helper.calculate as calculate
@@ -17,3 +19,17 @@ show.say_hello(guest="Thousand")
 show.introduce(name="Thousand",age=30,gender="Male")
 print(calculate.get_max(number=number))
 print(calculate.get_distance(point1=(3,5),point2=(6,1)))
+"""
+
+class Warrior:
+    def __init__(self,name,gender):
+        self.level = 1
+        self.exp = 0
+        self.n = name
+        self.g = gender
+        self.weapon = 'sword'
+        print("This is Warrior Class.")
+
+
+warrior1 = Warrior(name="Thousand",gender="Male")
+warrior2 = Warrior(name="Allie",gender="Female")
